d4398fb70e412c981dba040557b4c2f658d9f8fd
Krzysztof Kuczyński <K.Kuczynski@wasko.pl>
2020-07-02 09:00:09 +0200

    dodanie wstepnych getterow i setterow

--- PetAdopt_backend/src/main/java/com/petAdopt/model/dto/AdoptionHousesDto.java
+++ PetAdopt_backend/src/main/java/com/petAdopt/model/dto/AdoptionHousesDto.java
@@ -1,6 +1,7 @@
 package com.petAdopt.model.dto;
 
 public class AdoptionHousesDto {
+private
     int id;
     String address;
     String city;
@@ -8,4 +9,47 @@ public class AdoptionHousesDto {
     int user_id;
     int pets_id;
     String conditions;
+public
+    int getId(){
+        return this.id;
+    }
+    String getAddress(){
+        return this.address;
+    }
+    String getCity(){
+        return this.city;
+    }
+    String getPostcode(){
+        return this.postcode;
+    }
+    int getUser_id(){
+        return this.user_id;
+    }
+    int getPets_id(){
+        return this.pets_id;
+    }
+    String getConditions(){
+        return this.conditions;
+    }
+    void seId(){
+
+    }
+    void setAddress(){
+
+    }
+    void setCity(){
+
+    }
+    void setPostcode(){
+
+    }
+    void setUser_id(){
+
+    }
+    void setPets_id(){
+
+    }
+    void setConditions(){
+
+    }
 }
--- PetAdopt_backend/src/main/java/com/petAdopt/model/dto/UsersDto.java
+++ PetAdopt_backend/src/main/java/com/petAdopt/model/dto/UsersDto.java
@@ -3,10 +3,48 @@ package com.petAdopt.model.dto;
 import java.sql.Timestamp;
 
 public class UsersDto {
+private
     int id;
     String full_name;
     Timestamp created_at;
     int role; //0 - admin, 1 - employee, 2 - common user
     String login;
     String pass;
+public
+    int getId(){
+        return this.id;
+    }
+    String getFull_name(){
+        return this.full_name;
+    }
+    Timestamp getCreated_at(){
+        return this.created_at;
+    }
+    int getRole(){
+        return this.role;
+    }
+    String getLogin(){
+        return this.login;
+    }
+    String getPass(){
+        return this.pass;
+    }
+    void setId(){
+
+    }
+    void setFull_name(){
+
+    }
+    void setCreated_at(){
+
+    }
+    void setRole(){
+
+    }
+    void setLogin(){
+
+    }
+    void setPass(){
+
+    }
 }
