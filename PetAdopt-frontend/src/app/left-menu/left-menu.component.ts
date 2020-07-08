8521894ddfdb2619e566c7254dbc9d4890a8b5cb
Krzysztof Kuczyński <K.Kuczynski@wasko.pl>
2020-07-08 13:24:53 +0200

    left menu now shows and hides on mouseover

--- PetAdopt-frontend/src/app/left-menu/left-menu.component.ts
+++ PetAdopt-frontend/src/app/left-menu/left-menu.component.ts
@@ -8,23 +8,30 @@ import { Component, OnInit } from '@angular/core';
 export class LeftMenuComponent implements OnInit {
   public admin = true;
   public employee = true;
+  public isShown = false;
   constructor() { }
 
-  onMouseOverLeft(){
-    console.log('mouse on bar');
-}
+  onMouseOverLeft() {
+  
+      console.log('mouse on bar');
+      document.getElementById('menu').hidden = false;
+      document.getElementById('leftBar').style.transform = 'translate(700%,0%)';
+      this.isShown = true;
 
-onMouseOverRight(){
-  console.log('mouse on invisible bar');
-}
-  public showMenu() {
-  console.log('showmenu');
-}
-  public hideMenu() {
-  console.log('hidemenu');
-}
+  }
 
-ngOnInit(): void {
-}
+  onMouseOverRight() {
+    if (this.isShown) {
+      console.log('mouse on invisible bar');
+      document.getElementById('menu').hidden = true;
+      document.getElementById('leftBar').style.transform = 'translate(0%,0%)';
+      this.isShown = false;
+    }
+  }
+
+
+  ngOnInit(): void {
+    document.getElementById('menu').hidden = true;
+  }
 
 }
