176854d8fb4ff7ea6d6f5586ec1e141152e1aee2
Krzysztof Kuczyński <K.Kuczynski@wasko.pl>
2020-07-08 12:42:54 +0200

    added mouseover handling for left menu bar, will be used to unfold the menu

--- PetAdopt-frontend/src/app/left-menu/left-menu.component.ts
+++ PetAdopt-frontend/src/app/left-menu/left-menu.component.ts
@@ -6,12 +6,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./left-menu.component.css']
 })
 export class LeftMenuComponent implements OnInit {
-
+  public admin = true;
+  public employee = true;
   constructor() { }
 
-  public admin = true;
-  public employee = false;
-  ngOnInit(): void {
-  }
+  onMouseOverLeft(){
+    console.log('mouse on bar');
+}
+
+onMouseOverRight(){
+  console.log('mouse on invisible bar');
+}
+  public showMenu() {
+  console.log('showmenu');
+}
+  public hideMenu() {
+  console.log('hidemenu');
+}
+
+ngOnInit(): void {
+}
 
 }
